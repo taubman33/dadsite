90552aba6294bd0b01accc2be5dfe05294bbd5af
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-10-26 12:20:50 -0400

    starting up

---
+++ dad-site/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+ return (
+    <div className ='app-header'
+          style = {{backgroundColor:"red", width:"100%", top:"0"}}>
+        <h1>Header</h1>
+
+    </div>
+
+
+ )
+}
+
+
+export default Header 
+
